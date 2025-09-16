3d827c80ee14e6d1c80dc0b361514627f99280a7
mm <mmmdev2003@gmail.com>
2025-09-16 10:26:39 +0300

    подготовил бота к запуску

--- internal/repo/state/query.py
+++ internal/repo/state/query.py
@@ -1,21 +1,21 @@
 create_state = """
-INSERT INTO dialog_states (tg_chat_id)
+INSERT INTO user_states (tg_chat_id)
 VALUES (:tg_chat_id)
 RETURNING id;
 """
 
 state_by_id = """
-SELECT * FROM dialog_states
+SELECT * FROM user_states
 WHERE tg_chat_id = :tg_chat_id;
 """
 
 update_state_status = """
-UPDATE dialog_states
+UPDATE user_states
 SET status = :status
 WHERE id = :state_id;
 """
 
 delete_state_by_tg_chat_id = """
-DELETE FROM dialog_states
+DELETE FROM user_states
 WHERE tg_chat_id = :tg_chat_id;
 """
