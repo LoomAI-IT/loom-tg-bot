dd05a2260c6d82468324199d5bcc3f8c10cb18f5
mm <mmmdev2003@gmail.com>
2025-09-21 21:46:46 +0300

    Сделал несколько картинок на модерации

--- internal/model/client/kontur_organization.py
+++ internal/model/client/kontur_organization.py
@@ -6,7 +6,7 @@ class Organization:
     id: int
 
     name: str
-    rub_balance: int
+    rub_balance: str
     autoposting_moderation: bool
     video_cut_description_end_sample: str
     publication_text_end_sample: str
