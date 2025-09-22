ad247867bb3c43677722897616fa04c3916a970c
mm <mmmdev2003@gmail.com>
2025-09-22 21:57:23 +0300

    dev

--- internal/app/tg/app.py
+++ internal/app/tg/app.py
@@ -167,6 +167,7 @@ def include_dialogs(
 def include_db_handler(app: FastAPI, db: interface.IDB, prefix):
     app.add_api_route(prefix + "/table/create", create_table_handler(db), methods=["GET"])
     app.add_api_route(prefix + "/table/drop", drop_table_handler(db), methods=["GET"])
+    app.add_api_route(prefix+"/health", heath_check_handler(), methods=["GET"])
 
 
 def create_table_handler(db: interface.IDB):
@@ -177,6 +178,13 @@ def create_table_handler(db: interface.IDB):
             raise err
 
     return create_table
+
+def heath_check_handler():
+    async def heath_check():
+        return "ok"
+
+    return heath_check
+
 
 
 def drop_table_handler(db: interface.IDB):
