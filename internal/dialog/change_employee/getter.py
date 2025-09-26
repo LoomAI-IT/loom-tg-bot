98da29d45c00c510243ff4cec159788d6bdf4ae6
mm <mmmdev2003@gmail.com>
2025-09-26 20:18:16 +0300

    dev

--- internal/dialog/change_employee/getter.py
+++ internal/dialog/change_employee/getter.py
@@ -203,7 +203,6 @@ class ChangeEmployeeGetter(interface.IChangeEmployeeGetter):
                     "approved_publication_count": approved_publication_count,
                     "has_moderated_publications": bool(rejected_publication_count or approved_publication_count),
                 }
-                print(data, flush=True)
 
                 span.set_status(Status(StatusCode.OK))
                 return data
