8e782c41b8122f9bfbf5596b8ae71df3886550e9
mm <mmmdev2003@gmail.com>
2025-10-03 08:10:04 +0300

    change org model

--- internal/model/client/loom_organization.py
+++ internal/model/client/loom_organization.py
@@ -4,11 +4,15 @@ from dataclasses import dataclass
 @dataclass
 class Organization:
     id: int
-
     name: str
     rub_balance: str
-    autoposting_moderation: bool
     video_cut_description_end_sample: str
     publication_text_end_sample: str
-
+    tone_of_voice: list[str]
+    brand_rules: list[str]
+    compliance_rules: list[str]
+    audience_insights: list[str]
+    products: list[dict]
+    locale: dict
+    additional_info: list[str]
     created_at: str
