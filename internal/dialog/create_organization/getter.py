d3dcd49b0b219239bca37b04ad36302819c9a688
mm <mmmdev2003@gmail.com>
2025-10-24 08:39:45 +0300

    fix middleware

--- internal/dialog/create_organization/getter.py
+++ internal/dialog/create_organization/getter.py
@@ -58,7 +58,6 @@ class CreateOrganizationGetter(interface.ICreateOrganizationGetter):
                     history=history,
                     system_prompt=system_prompt,
                     temperature=1,
-                    enable_web_search=False
                 )
 
             message_to_user = llm_response_json["message_to_user"]
