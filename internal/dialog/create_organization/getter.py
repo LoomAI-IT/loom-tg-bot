4b4606422f2e22ea7f0d9fcc8b2a376a682d1581
mm <mmmdev2003@gmail.com>
2025-10-24 12:36:50 +0300

    fix middleware

--- internal/dialog/create_organization/getter.py
+++ internal/dialog/create_organization/getter.py
@@ -79,6 +79,9 @@ class CreateOrganizationGetter(interface.ICreateOrganizationGetter):
         return data
 
     def _format_message(self, message_to_user: str) -> str:
+        message_to_user = message_to_user.replace("</details>\n\n", "</details>")
+        message_to_user = message_to_user.replace("</details>\n", "</details>")
+        message_to_user = message_to_user.replace("</details>", "</details><br><br>")
         message_to_user = message_to_user.replace("</li>\n", "</li>")
         message_to_user = message_to_user.replace("\n", "<br>")
         return re.sub(r'</details>(?:\s*</?br\s*/?>)?', r'</details><br><br>', message_to_user)
