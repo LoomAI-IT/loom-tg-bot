ccef80ba450d0e9b9149843f87d6918d155dd8c8
mm <mmmdev2003@gmail.com>
2025-10-29 07:35:56 +0300

    fix middleware

--- internal/dialog/brief/create_category/helpers/category_manager.py
+++ internal/dialog/brief/create_category/helpers/category_manager.py
@@ -55,3 +55,30 @@ class CategoryManager:
             created_at="-"
         )
         dialog_manager.dialog_data["category_data"] = category.to_dict()
+
+    async def create_category(
+            self,
+            organization_id: int,
+            category_data: dict
+    ) -> int:
+        category_id = await self.loom_content_client.create_category(
+            organization_id=organization_id,
+            name=category_data.get("name"),
+            hint=category_data.get("hint"),
+            goal=category_data.get("goal"),
+            tone_of_voice=category_data.get("tone_of_voice"),
+            brand_rules=category_data.get("brand_rules"),
+            creativity_level=category_data.get("creativity_level"),
+            audience_segment=category_data.get("audience_segment"),
+            len_min=category_data.get("len_min"),
+            len_max=category_data.get("len_max"),
+            n_hashtags_min=category_data.get("n_hashtags_min"),
+            n_hashtags_max=category_data.get("n_hashtags_max"),
+            cta_type=category_data.get("cta_type"),
+            cta_strategy=category_data.get("cta_strategy"),
+            good_samples=category_data.get("good_samples"),
+            bad_samples=category_data.get("bad_samples"),
+            additional_info=category_data.get("additional_info"),
+            prompt_for_image_style=category_data.get("prompt_for_image_style"),
+        )
+        return category_id
